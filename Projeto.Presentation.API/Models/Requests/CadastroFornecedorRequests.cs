e308a56ed3f4668d2dc52287e4271cfb43769abb
marcio henrique <mhmuzy@embratel.com.br>
2020-06-12 08:51:13 -0300

    Treinamento

--- Projeto.Presentation.API/Models/Requests/CadastroFornecedorRequests.cs
+++ Projeto.Presentation.API/Models/Requests/CadastroFornecedorRequests.cs
@@ -6,11 +6,15 @@ using System.ComponentModel.DataAnnotations; //validações
 
 namespace Projeto.Presentation.API.Models.Requests
 {
-    public class CadastroProdutoRequests
+    public class CadastroFornecedorRequests
     {
         [MinLength(6, ErrorMessage = "Por favor, informe no mínimo {1} caracteres.")]
         [MaxLength(150, ErrorMessage = "Por favor, informe no máximo {1} caracteres.")]
         [Required(ErrorMessage = "Por favor, informe o nome do fornecedor.")]
         public string Nome { get; set; }
+
+        [MinLength(14, ErrorMessage = "Por favor, informe o Cnpj em {1} caracteres.")]
+        [MaxLength(14)]
+        public string Cnpj { get; set; }
     }
 }
