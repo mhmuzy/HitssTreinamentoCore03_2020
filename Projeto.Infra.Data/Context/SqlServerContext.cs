bb7844d9e4bd2b8fff961ea79104b39a4938f4d3
marcio henrique <mhmuzy@embratel.com.br>
2020-07-08 17:01:03 -0300

    Treinamento

---
+++ Projeto.Infra.Data/Context/SqlServerContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Projeto.Infra.Data.Entities;
+using Projeto.Infra.Data.Mappings;
+
+
+namespace Projeto.Infra.Data.Context
+{
+    //REGRA 1) Herdar DbContext
+    public class SqlServerContext : DbContext
+    {
+        //REGRA 2) Construtor para inicializar a classe DbContext
+        public SqlServerContext(DbContextOptions<SqlServerContext> options)
+            : base(options) //construtor da superclasse (DbContext)
+        {
+
+        }
+
+        //REGRA 3) Declarar uma propriedade DbSet para cada entidade do projeto
+        public DbSet<Fornecedor> Fornecedores { get; set; } //CRUD..
+        public DbSet<Produto> Produtos { get; set; } //CRUD..
+
+        //REGRA 4)
+    }
+}
