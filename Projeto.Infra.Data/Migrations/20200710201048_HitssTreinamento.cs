412fd2064f45197cfd5e11b5bbd29e4d773eb05d
marcio henrique <mhmuzy@embratel.com.br>
2020-07-10 17:21:26 -0300

    Treinamento

---
+++ Projeto.Infra.Data/Migrations/20200710201048_HitssTreinamento.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Projeto.Infra.Data.Migrations
+{
+    public partial class HitssTreinamento : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fornecedor",
+                columns: table => new
+                {
+                    IdFornecedor = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(maxLength: 150, nullable: false),
+                    Cnpj = table.Column<string>(maxLength: 14, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fornecedor", x => x.IdFornecedor);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    IdProduto = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(maxLength: 150, nullable: true),
+                    Preco = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    Quantidade = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.IdProduto);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fornecedor_Cnpj",
+                table: "Fornecedor",
+                column: "Cnpj",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fornecedor");
+
+            migrationBuilder.DropTable(
+                name: "Produto");
+        }
+    }
+}
