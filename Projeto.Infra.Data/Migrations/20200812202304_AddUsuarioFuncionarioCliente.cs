5b38ab073f6f20e3dd57ccefa3f6e1b6045358ae
marcio henrique <mhmuzy@embratel.com.br>
2020-08-12 17:42:40 -0300

    Treinamento

---
+++ Projeto.Infra.Data/Migrations/20200812202304_AddUsuarioFuncionarioCliente.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Projeto.Infra.Data.Migrations
+{
+    public partial class AddUsuarioFuncionarioCliente : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IdFornecedor",
+                table: "Produto",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    IdCliente = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(maxLength: 150, nullable: false),
+                    Cpf = table.Column<string>(maxLength: 11, nullable: false),
+                    DataNascimento = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.IdCliente);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Funcionario",
+                columns: table => new
+                {
+                    IdFuncionario = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(maxLength: 150, nullable: false),
+                    Cpf = table.Column<string>(maxLength: 11, nullable: false),
+                    DataContratacao = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Funcionario", x => x.IdFuncionario);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    IdUsuario = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(maxLength: 150, nullable: false),
+                    Email = table.Column<string>(maxLength: 100, nullable: false),
+                    Senha = table.Column<string>(maxLength: 50, nullable: false),
+                    DataCriacao = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.IdUsuario);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fornecedor_Nome",
+                table: "Fornecedor",
+                column: "Nome",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cliente_Cpf",
+                table: "Cliente",
+                column: "Cpf",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cliente_Nome",
+                table: "Cliente",
+                column: "Nome",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Funcionario_Cpf",
+                table: "Funcionario",
+                column: "Cpf",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Funcionario_Nome",
+                table: "Funcionario",
+                column: "Nome",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuario_Email",
+                table: "Usuario",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuario_Nome",
+                table: "Usuario",
+                column: "Nome",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+
+            migrationBuilder.DropTable(
+                name: "Funcionario");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Fornecedor_Nome",
+                table: "Fornecedor");
+
+            migrationBuilder.DropColumn(
+                name: "IdFornecedor",
+                table: "Produto");
+        }
+    }
+}
